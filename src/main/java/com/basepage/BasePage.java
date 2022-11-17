5b18c6a0bf5b9dde0d16a8774ef458f030176831
hoquenazmul <technazmul.hoque@gmail.com>
2022-11-16 23:32:55 -0500

    configured log4j, properties and webdriver on BasePage

--- src/main/java/com/basepage/BasePage.java
+++ src/main/java/com/basepage/BasePage.java
@@ -1,5 +1,60 @@
 package com.basepage;
 
-public class BasePage {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasePage {
+	public static Logger log;
+	public static Properties props;
+	public static WebDriver driver;
+	
+	public BasePage() {
+		log = LogManager.getLogger(BasePage.class);
+		
+		try {
+			props = new Properties();
+			String file_path = System.getProperty("user.dir") + "\\src\\main\\resources\\config.properties";
+			FileInputStream fis = new FileInputStream(file_path);
+			props.load(fis);
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void init() {
+		if (props.getProperty("browser").equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (props.getProperty("browser").equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			System.out.println("Driver not Found!");
+		}
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(props.getProperty("URL"));
+	}
+	
+	public static void main(String[] args) {
+		BasePage bp = new BasePage();
+		bp.init();
+	}
+	
+	
 }
