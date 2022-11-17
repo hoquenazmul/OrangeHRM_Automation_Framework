d1474d500fc1b05323078dae3710a6a3a83715d4
hoquenazmul <technazmul.hoque@gmail.com>
2022-11-16 22:50:16 -0500

    cucumber options added

--- src/test/java/com/runner/CucumberRunner.java
+++ src/test/java/com/runner/CucumberRunner.java
@@ -1,5 +1,17 @@
 package com.runner;
 
-public class CucumberRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+	plugin= {"pretty","json:target/cucumber.json" }, // for html= {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" },	
+	features = {".//features/"}, 
+    glue = {"com.stepdefs","com.hooks"}, 	
+    tags = "@smoke and @core_regression",
+	dryRun = false, 
+	monochrome = true
+)
+
+public class CucumberRunner extends AbstractTestNGCucumberTests {
 
 }
